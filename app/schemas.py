17e020ec163f8ff7c992dc08b122ce8191252531
mboynov <>
2024-05-23 10:35:43 +0300

    test

--- app/schemas.py
+++ app/schemas.py
@@ -6,6 +6,7 @@ class ChatRequest(BaseModel):
     message_id: Optional[str] = None
     userPromptUrlEncoded: Optional[str] = None
     systemPromptUrlEncoded: Optional[str] = None
+    llmModel: Optional[str] = None
     image: Optional[bool] = False
     image_url: Optional[str] = None
 
